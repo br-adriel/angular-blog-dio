b9324d316fbe91290b4f9d98bdfcb9ba3d9f9562
Adriel <adriel.fsantos@outlook.com>
2023-04-24 20:05:54 -0300

    Implementa leitura dinâmica de postagens

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
   @Input() cover: string = '';
   @Input() title: string = '';
   @Input() description: string = '';
+
+  private id: string | null = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => {
+      this.id = value.get('id');
+    });
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id === id)[0];
+
+    if (result) {
+      this.title = result.title;
+      this.cover = result.photo;
+      this.description = result.description;
+    }
+  }
 }
