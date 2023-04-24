0fa6be6400db6c2a1dd15ac64acca5c722528926
Adriel <adriel.fsantos@outlook.com>
2023-04-24 20:04:19 -0300

    Adiciona mockup de banco de dados

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,35 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo:
+      'https://ogimg.infoglobo.com.br/in/24907109-c86-bcf/FT1086A/760/avatar-a-lenda-de-aang.jpg',
+    title:
+      'Netflix divulga data de lançamento do trailer da série live-action de Avatar: A Lenda de Aang',
+    description:
+      'Na última semana a netflix confirmou a data de lançamento do trailer da série live action',
+  },
+  {
+    id: '2',
+    photo:
+      'https://a-static.mlcdn.com.br/1500x1500/scythe-volumes-1-o-ceifador-2-a-nuvem-e-3-o-timbre-companhia-das-letras/mdesouzalivrariaepapelaria/14219973130/a7b3a98ed79a47d22a83ec79b1a827ed.jpeg',
+    title: 'O que esperar do quarto livro do Ceifador?',
+    description:
+      'Em novembro de 2022 "Gleanings" foi lançado, mas afinal sobre o que fala o livro?',
+  },
+  {
+    id: '3',
+    photo:
+      'https://uploads.metropoles.com/wp-content/uploads/2023/03/25180014/jogos-vorazes-a-cantiga-dos-passaros-e-das-serpentes.jpg',
+    title: 'Novo filme de Jogos Vorazes ganha teaser',
+    description:
+      'o quinto filme da franquia seguirá a história do quarto livro, onde acompanhamos a juventude do presidente Snow',
+  },
+  {
+    id: '4',
+    photo:
+      'https://s2.glbimg.com/mzpY3FvumHcUhKRdTMdarTcTGjU=/0x0:1400x975/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2022/w/I/Ah4LlIQjytJCtypGngZA/imagine-dragons.jpg',
+    title: 'Imagine Dragons anúncia nova turnê',
+    description:
+      'A banda postou no seu site oficial os locais e as datas, confira!',
+  },
+];
