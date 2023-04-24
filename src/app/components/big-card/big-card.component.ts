b89fbef5598fdd07746f49407e8a1ff74813a269
Adriel <adriel.fsantos@outlook.com>
2023-04-24 18:09:57 -0300

    Atualiza big-card com conteúdo dinâmico

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
-
+  @Input() cover: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
 }
